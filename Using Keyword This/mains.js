31d2004cafa61443e421beeea7e78a69a8d8e967
Sean <omerta201@yahoo.com>
2018-10-04 22:05:02 -0400

    added next section

--- Using Keyword This/mains.js
+++ Using Keyword This/mains.js
@@ -7,3 +7,13 @@ const robot = {
   };
   
   console.log(robot.provideInfo())
+
+//Do not use arrow functions for methods
+  const robot = {
+    energyLevel: 100,
+    checkEnergy: function() {
+      console.log(`Energy is currently at ${this.energyLevel}%.`)
+    }
+  }
+  
+  robot.checkEnergy();
